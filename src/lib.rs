efdd186cd9b1acef6474276af7acd6df01d26f49
Aidan Beggs <nadiasggeb001@gmail.com>
2020-11-18 14:30:26 -0500

    Code formatting!

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,8 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 
 #[no_mangle]
-pub extern fn rust_main() {
-	vga_buffer::print_something();
+pub extern "C" fn rust_main() {
+    vga_buffer::print_something();
 
     // ATTENTION: we have a very small stack and no guard page
 
@@ -16,28 +16,24 @@ pub extern fn rust_main() {
 
     let mut hello_colored = [color_byte; 24];
     for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
+        hello_colored[i * 2] = *char_byte;
     }
 
     // write `Hello World!` to the center of the VGA text buffer
     let buffer_ptr = (0xb8000 + 1988) as *mut _;
     unsafe { *buffer_ptr = hello_colored };
 
-    loop{}
+    loop {}
 }
 
-pub fn print_something() {
-
-}
+pub fn print_something() {}
 
 #[lang = "eh_personality"]
 #[no_mangle]
-extern "C" fn rust_eh_personality() {
-
-}
+extern "C" fn rust_eh_personality() {}
 
 #[panic_handler]
 #[no_mangle]
 extern "C" fn panic_handler(_info: &PanicInfo) -> ! {
-	loop { }
+    loop {}
 }
